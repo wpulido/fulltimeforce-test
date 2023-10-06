1ebe344fb21cf620b1770535bc52a38d57c5862e
Wistre <wistre@raxo.co>
2023-10-06 16:40:02 -0400

    Frontend skeleton added

---
+++ apps/frontend/components/commit/Commit.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Commit() {
+  return (
+    <div className="border-[1px] bg-inherit rounded-lg p-5 border-white/20 flex justify-between align-center">
+      <div>
+        <h2>Commit message here</h2>
+        <p>Commit author</p>
+      </div>    
+      <p>Commit date here</p>
+    </div>
+  )
+}
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,15 +1,18 @@
-
+import { useState } from "react"
+import Commit from "../components/commit/Commit"
 
 function App() {
-
   return (
-    <>
-      <button className="bg-white text-black py-3 px-5 rounded-md" onClick={async() => {
-        const response = await fetch('/api');
-        const data = await response.text();
-        console.log(data);
-      }}>Click me</button>
-    </>
+    <div className="container mx-auto">
+      <div className="text-center mt-8">
+        <h1 className="font-semibold">Git repository commits app</h1>
+        <p className="text-lg mt-3">This app shows all commits made to <a target="_blank" rel="noopener noreferrer" className="text-white underline hover:text-white" href="https://github.com/wpulido/fulltimeforce-test">this</a> repository.</p>
+      </div>
+
+      <div id="list" className="flex flex-col">
+        <Commit />
+      </div>
+    </div>
   )
 }
 
