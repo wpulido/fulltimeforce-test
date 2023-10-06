1dec71cee19e1810b0c66cf2ad93ddb102b72f01
Wistre <wistre@raxo.co>
2023-10-06 16:40:55 -0400

    fix commit component layout

--- apps/frontend/components/commit/Commit.tsx
+++ apps/frontend/components/commit/Commit.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Commit() {
   return (
-    <div className="border-[1px] bg-inherit rounded-lg p-5 border-white/20 flex justify-between align-center">
+    <div className="border-[1px] bg-inherit rounded-lg p-5 border-white/20 flex justify-between items-center">
       <div>
         <h2>Commit message here</h2>
         <p>Commit author</p>
